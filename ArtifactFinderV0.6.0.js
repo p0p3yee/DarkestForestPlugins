5997979f48d1f5ba4521de6103a9237333cce5bb
p0p3yee <owner@kagome100.com>
2021-06-01 02:52:52 +0800

    Add prospect & energy check for prospect planet

--- ArtifactFinderV0.6.0.js
+++ ArtifactFinderV0.6.0.js
@@ -10,14 +10,24 @@ render(container) {
 
   const find = () => {
     container.innerHTML = ""
+    let message = document.createElement("div")
     df.getMyPlanets().filter(p => p.location && df.isPlanetMineable(p)).forEach(x => {
-      let message = document.createElement("div")
       let newLoc = document.createElement("button")
       newLoc.style.width = '100%';
       newLoc.style.marginBottom = '10px';
-      newLoc.innerText = x.hasTriedFindingArtifact ? "Found, click to view planet" : "Not Found"
+      const enoughEnergy = x.energy >= x.energyCap * 0.95
+      newLoc.innerText = x.prospectedBlockNumber == null ? enoughEnergy ? "Not Been Prospected" : "Not enought energy to prospect" : x.hasTriedFindingArtifact ? "Found, click to view planet" : "Not Been Found"
       // newLoc.disabled = x.hasTriedFindingArtifact
-      newLoc.onclick = () => {
+      newLoc.onclick = async () => {
+        if (x.prospectedBlockNumber == null) {
+          if (enoughEnergy) {
+            await df.prospectPlanet(x.locationId)
+          } else {
+            ui.setSelectedId(x.locationId)
+            message.innerHTML = "Not enough energy to prospect this planet"
+            return
+          }
+        }
         ui.setSelectedId(x.locationId)
         if (x.hasTriedFindingArtifact) {
           return
@@ -28,9 +38,10 @@ render(container) {
   
       container.appendChild(newLoc)
       container.appendChild(refreshBtn)
-      container.appendChild(message)
+      
       // container.innerHTML += "<br />"
     })
+    container.appendChild(message)
   }
 
   refreshBtn.onclick = () => find()
