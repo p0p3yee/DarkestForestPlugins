8a58430c67a0ada1330b6fc9867b7ce5127a8d11
p0p3yee <owner@kagome100.com>
2021-06-01 02:58:13 +0800

    Add hide all found aritfact planets

--- ArtifactFinderV0.6.0.js
+++ ArtifactFinderV0.6.0.js
@@ -16,31 +16,35 @@ render(container) {
       newLoc.style.width = '100%';
       newLoc.style.marginBottom = '10px';
       const enoughEnergy = x.energy >= x.energyCap * 0.95
-      newLoc.innerText = x.prospectedBlockNumber == null ? enoughEnergy ? "Not Been Prospected" : "Not enought energy to prospect" : x.hasTriedFindingArtifact ? "Found, click to view planet" : "Not Been Found"
+      const isFound = x.hasTriedFindingArtifact
+      if (isFound) return
+      newLoc.innerText = x.prospectedBlockNumber == null ? enoughEnergy ? "Not Been Prospected" : "Not enought energy to prospect" : "Not Been Found"
       // newLoc.disabled = x.hasTriedFindingArtifact
       newLoc.onclick = async () => {
-        if (x.prospectedBlockNumber == null) {
-          if (enoughEnergy) {
-            await df.prospectPlanet(x.locationId)
-          } else {
-            ui.setSelectedId(x.locationId)
-            message.innerHTML = "Not enough energy to prospect this planet"
-            return
-          }
-        }
         ui.setSelectedId(x.locationId)
         if (x.hasTriedFindingArtifact) {
           return
         }
+
+        if (x.prospectedBlockNumber == null) {
+          if (enoughEnergy) {
+            message.innerHTML = "Prospecting Planet..."
+            await df.prospectPlanet(x.locationId)
+          } else {
+            message.innerHTML = `Not enough energy to prospect this planet<br/>Energy Required: ${x.energyCap * 0.95}<br/>Current Energy: ${x.energy}`
+            return
+          }
+        }
+        
         df.findArtifact(x.locationId)
         message.innerHTML = "Finding Artifact..."
       }
   
       container.appendChild(newLoc)
-      container.appendChild(refreshBtn)
       
       // container.innerHTML += "<br />"
     })
+    container.appendChild(refreshBtn)
     container.appendChild(message)
   }
 
